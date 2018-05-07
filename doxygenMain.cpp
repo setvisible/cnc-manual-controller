843cb7d4d5d9a594fb0028049efa7e230be77e78
setvisible <sebastien.vavassori@gmail.com>
2018-05-07 19:26:03 +0200

    Update documentation

--- doxygenMain.cpp
+++ doxygenMain.cpp
@@ -6,13 +6,15 @@
  *
  * \section main-introduction Introduction
  *
- * CNC Manual Controller is a C++ programme that controls CNC machine
- * on Windows, Mac OS X and Linux.
+ * CNC Manual Controller (CNC-MC) is an open-source solution for 
+ * controlling CNC machines, for Windows, Mac OS X and Linux.
  * 
  * Its core is developed in C++, based on the Qt framework.
  *
  * https://www.qt.io/
  *
+ * \section quick-overview Quick Overview
+ * 
  * This programme was initially designed for controlling
  * a CNC machine driven by a Velleman VM110N experimental card
  * through an USB port.
